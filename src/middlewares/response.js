1afe174de97fb61b8f50a338df04d5de560350dd
Yan Almeida <yanalmeidagarcia@gmail.com>
2020-07-08 20:51:16 -0300

    Respostas padrões da API

---
+++ src/middlewares/response.js
@@ -0,0 +1,79 @@
+const TYPE_JSON = 'application/json';
+const STATUS_CODE_OK = 200;
+const STATUS_CODE_BAD_RESQUEST = 400;
+const STATUS_CODE_UNAUTHORIZED = 401;
+const STATUS_CODE_NOT_FOUND = 404;
+const STATUS_CODE_SERVER_ERROR = 500;
+
+const jsonOK = function(data, message, metadata) {
+    const status = STATUS_CODE_OK;
+
+    message = (message) ? message : 'Successful request.';
+    metadata = (metadata) ? metadata : {};
+
+    this.status(status);
+    this.type(TYPE_JSON);
+
+    return this.json({ message, data, metadata, status: status });
+};
+
+const jsonBadRequest = function(data, message, metadata) {
+    const status = STATUS_CODE_BAD_RESQUEST;
+
+    message = (message) ? message : 'Bad resquest.';
+    metadata = (metadata) ? metadata : {};
+
+    this.status(status);
+    this.type(TYPE_JSON);
+
+    return this.json({ message, data, metadata, status: status });
+};
+
+const jsonUnauthorized = function(data, message, metadata) {
+    const status = STATUS_CODE_UNAUTHORIZED;
+
+    message = (message) ? message : 'Unauthorized.';
+    metadata = (metadata) ? metadata : {};
+
+    this.status(status);
+    this.type(TYPE_JSON);
+
+    return this.json({ message, data, metadata, status: status });
+};
+
+const jsonNotFound = function(data, message, metadata) {
+    const status = STATUS_CODE_NOT_FOUND;
+
+    message = (message) ? message : 'Not found.';
+    metadata = (metadata) ? metadata : {};
+
+    this.status(status);
+    this.type(TYPE_JSON);
+
+    return this.json({ message, data, metadata, status: status });
+};
+
+const jsonServerError = function(data, message, metadata) {
+    const status = STATUS_CODE_SERVER_ERROR;
+
+    message = (message) ? message : 'Server error.';
+    metadata = (metadata) ? metadata : {};
+
+    this.status(status);
+    this.type(TYPE_JSON);
+
+    return this.json({ message, data, metadata, status: status });
+};
+
+const response = (req, res, next) => {
+
+    res.jsonOK = jsonOK;
+    res.jsonBadRequest = jsonBadRequest;
+    res.jsonUnauthorized = jsonUnauthorized;
+    res.jsonNotFound = jsonNotFound;
+    res.jsonServerError = jsonServerError;
+
+    next();
+}
+
+module.exports = response;
