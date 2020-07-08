0ab9f5970bce962789a44e53590d7f3d6faa143f
Yan Almeida <yanalmeidagarcia@gmail.com>
2020-07-08 19:37:22 -0300

    REST client config

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ const authController = require('./controllers/auth');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-
 app.use('/auth', authController);
 
 // request & response
