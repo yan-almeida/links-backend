044676613208ce7dd6167dee4e10642eb4c02401
Yan Almeida <yanalmeidagarcia@gmail.com>
2020-07-14 19:02:09 -0300

    excluded jwt config

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,18 +1,32 @@
 const { verifyJwt } = require('../helpers/jwt');
 
 const checkJwt = (req, res, next) => {
+    // /auth/sign-in; /auth/sign-up >> rotas a serem excluidas da verificação
+    const { url: path } = req;
+
+    const excludedPaths = [
+        '/auth/sign-in',
+        '/auth/sign-up'
+    ];
+    const isExcluded = !!excludedPaths.find((p) => p.startsWith(path));
+    if (isExcluded) return next();
+
     let token = req.headers['authorization'];
     token = token ? token.slice(7, token.length) : null;
 
-    if (!token) return res.jsonUnauthorized(null, 'Invalid token');
-
+    if (!token) {
+        console.log('TOKEN: ', token)
+        return res.jsonUnauthorized(null, 'Invalid token - token');
+    }
+    // 1:21h
     try {
         const decoded = verifyJwt(token);
         req.accountId = decoded.id;
 
         next();
     } catch (error) {
-        return res.jsonUnauthorized(null, 'Invalid token');
+        console.log(error);
+        return res.jsonUnauthorized(null, 'Invalid token - catch');
     }
 };
 
