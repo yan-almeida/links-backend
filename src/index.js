d2e1129143d942372c4b2d83d61cca8f13aa7103
keys8410 <keys8410@gmail.com>
2020-07-19 17:25:53 -0300

    cors adicionado

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./models');
-const response = require('./middlewares/response');
-const checkJwt = require('./middlewares/jwt');
+const express = require("express");
+const cors = require("cors");
+const db = require("./models");
+const response = require("./middlewares/response");
+const checkJwt = require("./middlewares/jwt");
 
-const authController = require('./controllers/auth');
-const linkController = require('./controllers/link');
+const authController = require("./controllers/auth");
+const linkController = require("./controllers/link");
 
 const app = express();
 
-app.user(cors);
+app.use(cors);
 app.use(response);
 app.use(checkJwt);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/auth', authController); // rota de autenticação
-app.use('/link', linkController); // rota de autenticação
+app.use("/auth", authController); // rota de autenticação
+app.use("/link", linkController); // rota de autenticação
 
 // request & response
-app.get('/', (req, res) => {
-    return res.json('API rodando...');
+app.get("/", (req, res) => {
+  return res.json("API rodando...");
 });
 
 /* init db */
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log('Rodando na porta 3001');
-    })
+  app.listen(3001, () => {
+    console.log("Rodando na porta 3001");
+  });
 });
